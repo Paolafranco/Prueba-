9281a1802dd389abcb86ea92a0ba1e3653f10977
Paolafranco <55416764+Paolafranco@users.noreply.github.com>
2020-02-11 11:57:42 -0500

    Add files via upload

---
+++ pao prueba/pokemon.js
@@ -0,0 +1,61 @@
+class Pokemon {
+  accionDeVuelo;
+  accionDeDefensa;
+
+  setaccionVuelo(accionDeVuelo){
+      this.accionDeVuelo = accionDeVuelo
+  }
+
+  setaccionDefensa( accionDeDefensa){
+      this. accionDeDefensa =  accionDeDefensa
+  }
+
+  
+  comfly(){
+      this.accionDeVuelo.fly()
+    }
+  
+  comDefending(){
+      this. accionDeDefensa.defending()
+    }
+
+}
+
+class Vuelo extends Pokemon{
+  display(){
+    console.log('nooooooooo')
+  }
+}
+
+
+class Charning extends Pokemon{
+  display(){
+    console.log('noooo soy')
+  }
+}
+
+
+class VueloAlto{
+  fly(){
+    console.log('Estoyyyyy Volandooooo')
+  }
+}
+
+class VueloRapidin{
+  fly(){
+    console.log('Estoy Vuela Vuelaaaa')
+  }
+}
+
+class DefensaEscudo{
+  defending(){
+    console.log('defensaaaaaaa')
+  }
+}
+
+class DefensaChick{
+  defending(){
+    console.log('defensaaaa and you')
+  }
+}
+
